1fe7c8815f3a2d0e4ba3d181b403ba5c45b80e93
grace kurui <gracekurui123@gmail.com>
2023-05-30 16:31:44 +0300

    for each array method

--- app.js
+++ app.js
@@ -13,6 +13,21 @@
 // console.log(nine);
 
 //filter array method
+// let people = [
+//   { name: "grace", age: 29, profession: "teacher", gender: "female", id: 1 },
+//   { name: "race", age: 27, profession: "sales", gender: "male", id: 2 },
+//   { name: "mace", age: 24, profession: "police", gender: "female", id: 3 },
+//   { name: "bace", age: 22, profession: "nurse", gender: "male", id: 4 },
+//   { name: "pace", age: 98, profession: "driver", gender: "female", id: 5 },
+// ];
+
+// let nine = people.filter(function (any) {
+//   return any.id === 3;
+// });
+// console.log(nine);
+
+//  start of for each array method
+
 let people = [
   { name: "grace", age: 29, profession: "teacher", gender: "female", id: 1 },
   { name: "race", age: 27, profession: "sales", gender: "male", id: 2 },
@@ -21,7 +36,7 @@ let people = [
   { name: "pace", age: 98, profession: "driver", gender: "female", id: 5 },
 ];
 
-let nine = people.filter(function (any) {
-  return any.id === 3;
+people.forEach(function (any) {
+  console.log(any.name);
 });
-console.log(nine);
+//  end of for each array method
