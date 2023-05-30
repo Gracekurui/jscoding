d869a626a4ce0234d1571cb6d6f9e47f0fca4a96
grace kurui <gracekurui123@gmail.com>
2023-05-30 16:25:57 +0300

    filter array method

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 //find array method
+// let people = [
+//   { name: "grace", age: 29, profession: "teacher", gender: "female", id: 1 },
+//   { name: "race", age: 27, profession: "sales", gender: "male", id: 2 },
+//   { name: "mace", age: 24, profession: "police", gender: "female", id: 3 },
+//   { name: "bace", age: 22, profession: "nurse", gender: "male", id: 4 },
+//   { name: "pace", age: 98, profession: "driver", gender: "female", id: 5 },
+// ];
+
+// let nine = people.find(function (any) {
+//   return any.id === 3;
+// });
+// console.log(nine);
+
+//filter array method
 let people = [
   { name: "grace", age: 29, profession: "teacher", gender: "female", id: 1 },
   { name: "race", age: 27, profession: "sales", gender: "male", id: 2 },
@@ -7,7 +21,7 @@ let people = [
   { name: "pace", age: 98, profession: "driver", gender: "female", id: 5 },
 ];
 
-let nine = people.find(function (any) {
+let nine = people.filter(function (any) {
   return any.id === 3;
 });
 console.log(nine);
